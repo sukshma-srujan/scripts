1535e864827dd8dfbf86d3f13a5689b8b3d2cb90
Jitendra Kumar <jk.bhu85@gmail.com>
2018-03-16 15:32:42 +0530

    Create generic-font-change.js
    This scripts changes fonts of a websites.

---
+++ generic-font-change.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Generic Font Change
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Change fonts of a websites
+// @author       Optimus Prime
+// @match        https://stackoverflow.com/*
+// @match        https://dzone.com/*
+// @match        https://www.tutorialspoint.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let ff = 'Fira Sans';
+    let ff2 = ff.split(' ').join('+');
+    
+    let mf = 'Fira Mono';
+    let mf2 = mf.split(' ').join('+');
+
+    let css = 
+`
+@import url('https://fonts.googleapis.com/css?family=${ff2}:400,400i,700,700i');
+@import url('https://fonts.googleapis.com/css?family=${mf}');
+* {
+  font-family: '${ff}', Ubuntu, 'Segoe UI', sans-serif !important;
+}
+code *, pre *{
+  font-family: '${mf}', Consolas, Courier, monospace !important;
+}
+`;
+    
+    let style = document.createElement('style');
+    style.innerHTML = css;
+    document.head.appendChild(style);
+})();
