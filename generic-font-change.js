a2576adcc83880636a5415a4142312cad61851f4
Jitendra Kumar <jk.bhu85@gmail.com>
2018-03-18 09:24:14 +0530

    Update generic-font-change.js
    Changed version to 0.2 to see what happens in Tampermonkey.

--- generic-font-change.js
+++ generic-font-change.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Generic Font Change
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Change fonts of a websites
 // @author       Optimus Prime
 // @match        https://stackoverflow.com/*
