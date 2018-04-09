7c5a3caf93e1084afd005aea6eb43b24924b2058
Jitendra Kumar <jk.bhu85@gmail.com>
2018-04-09 20:57:51 +0530

    Create w3-schools-change-mono-font.js

---
+++ w3-schools-change-mono-font.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         W3Schools: Change Mono Font
+// @namespace    https://github.com/optimus29
+// @version      1.0
+// @description  Change mono font
+// @author       Optimus Prime
+// @match        https://www.w3schools.com/*
+// @website      https://github.com/optimus29
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    function applyStyle() {
+        let mf = "Fira Mono";
+        let mf2 = sf.split(' ').join('+');
+        let css = `
+@import url('https://fonts.googleapis.com/css?family=${mf2}');
+.w3-code, .w3-code * {
+  font-family: '${mf}', consolas, monospace;
+}
+`;
+
+        let style = document.createElement("style");
+        style.innerHTML = css;
+
+        document.head.appendChild(style);
+    };
+
+    window.setTimeout(applyStyle, 4000);
+})();
