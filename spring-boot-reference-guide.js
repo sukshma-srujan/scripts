a8866c934b5d407257bc721c5e28cfc55383c232
Jitendra Kumar <jk.bhu85@gmail.com>
2018-04-19 19:40:09 +0530

    Update spring-boot-reference-guide.js

--- spring-boot-reference-guide.js
+++ spring-boot-reference-guide.js
@@ -17,18 +17,21 @@
     let mf2 = mf.split(' ').join('+');
 
     let css = 
-`
-@import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700,700i');
+`@import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,400i,700,700i');
 @import url('https://fonts.googleapis.com/css?family=Fira+Mono:400,700');
 * {
   font-family: 'Fira Sans', Ubuntu, 'Segoe UI', sans-serif !important;
+}
+html {
+  background-color: #171717;
 }
 body {
   width: 65%;
   padding: 0 3rem;
   margin: 0 auto;
-  box-shadow: 0 0 10px #aaa;
+  box-shadow: 0 0 10px #ccc;
   font-size: 16px;
+  background-color: #fffffa;
 }
 code, code *, pre, pre *
 {
@@ -43,15 +46,26 @@ code, pre {
   border: 0 !important;
   background: #f0f0f0;
 }
+div.important code, div.note code, div.tip code, div.warning code,
+code, code * {
+  color: #321 !important;
+}
 pre {
   padding: 1.5rem !important;
-  margin: -1.5rem 0;
+  margin: 1.5rem -1.5rem;
 }
 .sidebar {
   border: 0;
   padding:1px 1.5rem;
   background: #fffff4;
-  margin: -1.5rem 0;
+  margin: 1.5rem -1.5rem;
+}
+.hl-keyword {
+  font-weight: 400;
+  color: #466010;
+}
+h1, h2, h3, h4, h5, h6 {
+  color: #444 !important;
 }
 `;
     
