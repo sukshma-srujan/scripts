0bcb8018398a6203fa3b121972099e5fb0c38326
Jitendra Kumar <jk.bhu85@gmail.com>
2018-06-03 15:39:18 +0530

    Add tt element to have monospace font family

--- generic-font-change.js
+++ generic-font-change.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Generic Font Change
 // @namespace    https://github.com/optimus29
-// @version      1.7
+// @version      1.8
 // @description  Change fonts of websites
 // @author       Optimus Prime
 // @match        https://stackoverflow.com/*
