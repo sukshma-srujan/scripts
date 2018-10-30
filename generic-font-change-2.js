496656ad2b3d365e1af7ccbe7a92d6e1b0eb0c4f
Jitendra Kumar <jk.bhu85@gmail.com>
2018-10-30 19:31:44 +0530

    Update generic-font-change-2.js

--- generic-font-change-2.js
+++ generic-font-change-2.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Generic Font Change 2
 // @namespace    optimus29
-// @version      1.4
+// @version      1.5
 // @description  Change fonts of websites
 // @author       Optimus Prime
 // @match        https://www.bing.com/*
-// @match        https://xerox-jira.atlassian.net/*
 // @grant        none
 // ==/UserScript==
 
