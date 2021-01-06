414f0108216522dc720bfa78ea15815a535daab5
Jitendra Kumar <jk.bhu85@gmail.com>
2021-01-06 22:24:21 +0530

    Update 1337-full-page-content.js

--- torrent/1337-full-page-content.js
+++ torrent/1337-full-page-content.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         1337x Full Page Content
 // @namespace    https://github.com/optimus29
-// @version      1.0.2
+// @version      1.1.0
 // @description  Make main content show in full width of page
 // @author       Optimus Prime
 // @match        https://1337x.to/*
@@ -14,22 +14,60 @@
     const html = `
 <div class="full-content-ui">
   <div>
-    <label for="fullContentUiCb">
-		<input type="checkbox" id="fullContentUiCb">Full Content UI
-	</label>
+<label class="checkbox" for="fullContentUiCb">
+  <span class="checkbox__input">
+    <input type="checkbox" id="fullContentUiCb">
+    <span class="checkbox__control">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+        <path fill="none" stroke="currentColor" stroke-width="3" d="M1.73 12.91l6.37 6.37L22.79 4.59"></path></svg>
+    </span>
+  </span>
+  <span class="radio__label">Full Content UI</span>
+</label>
   </div>
 </div>
 `;
 
     var css = `
 .full-content-ui {
-  background: #171717;
   border-top: .06rem solid #373737;
-}
-.full-content-ui label {
-  color: white;
-  display: block;
   padding: 1rem;
+}
+.checkbox {
+  display: grid;
+  grid-template-columns: min-content auto;
+  grid-gap: 0.5em;
+  font-size: 1rem;
+  display: block;
+  color: white;
+}
+.checkbox__input input {
+  opacity: 0;
+  width: 1em;
+  height: 1em;
+  display: none;
+}
+.checkbox__control {
+  display: inline-grid;
+  width: 1em;
+  height: 1em;
+  border-radius: 0.25em;
+  border: 0.1em solid currentColor;
+}
+.checkbox__input {
+  grid-template-areas: "checkbox";
+}
+.radio__label {
+  padding-left: .5rem;
+  font-family: "Oswald Light";
+}
+.checkbox__control svg {
+  transition: transform 0.1s ease-in 25ms;
+  transform: scale(0);
+  transform-origin: bottom left;
+}
+.checkbox__input input:checked + .checkbox__control svg {
+  transform: scale(1);
 }
 `;
 
@@ -47,13 +85,13 @@
     document.head.appendChild(style);
 
     function makeContentFullPage() {
-        left.style.display = "none";
+        if (left) left.style.display = "none";
         rightElemClasses = right.getAttribute("class");
         right.setAttribute("class", ".col-12 .page-content");
     }
 
     function restoreUi() {
-        left.style.display = "block";
+        if (left) left.style.display = "block";
         right.setAttribute("class", rightElemClasses);
     }
 
