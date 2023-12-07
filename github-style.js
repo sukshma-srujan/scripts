c18104eb6aec120be81ce00edc0224fead4171a2
Jitendra Kumar <jk.bhu85@gmail.com>
2023-12-07 12:00:19 +0530

    Add google fonts to the style

--- github-style.js
+++ github-style.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github Style
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Github Style
 // @author       You
 // @match        https://github.com/*
@@ -13,7 +13,10 @@
     'use strict';
 
     const css =
-`:root {
+`
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,200;0,400;0,600;1,400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
+:root {
   --jk-ff: "IBM Plex Sans", sans-serif;
   --jk-mff: "Jetbrains Mono", ui-monospace,SFMono-Regular,SF Mono,Menlo,Consolas,Liberation Mono,monospace;
 }
@@ -34,5 +37,6 @@ pre {
 }`;
     const style = document.createElement('style');
     style.innerHTML = css;
+    style.setAttribute("title", "jk_github_style");
     document.body.appendChild(style);
 })();
