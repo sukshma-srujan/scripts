9c64b49570f58a7a85e370ccfc3b665db0de4137
Jitendra Kumar <jk.bhu85@gmail.com>
2023-12-11 19:56:14 +0530

    Update yt-app.js

--- yt-app.js
+++ yt-app.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JK YT App
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  Add native app like capability to have YouTube video play while browsing the page.
 // @author       You
 // @match        https://www.youtube.com/
@@ -34,6 +34,40 @@
       background: transparent;
       border: 1px solid rgba(255,0,0,.2);
     }
+    .jk-yt-wrapper {
+      position: fixed;
+      top: 1rem;
+      right: 1rem;
+      bottom: 1rem;
+      left: 1rem;
+      display: none;
+      background: black;
+      z-index: 10000;
+      box-sizing: border-box;
+      padding-top: 1rem;
+      box-shadow: 0 0 .5rem rgba(255, 255, 255, 0.25);
+      border-radius: .5rem;
+    }
+    .jk-yt-close-btn {
+      position: absolute;
+      right: 0px;
+      top: 0px;
+      border: 0px;
+      margin: 0px;
+      padding: 1rem;
+      line-height: 1;
+      border-radius: 100%;
+      background: rgba(255, 255, 255, 0.3);
+      width: 1rem;
+      height: 1rem;
+      box-sizing: content-box;
+      font-size: 1rem;
+      font-weight: bold;
+      cursor: pointer;
+    }
+    .jk-yt-close-btn:hover {
+      background: rgba(255, 255, 255, 0.4);
+    }
     `;
     const style = document.createElement('style');
     style.innerHTML = css;
@@ -44,40 +78,18 @@
     wrapper: document.createElement("div"),
     iframe: document.createElement("iframe"),
     init: function () {
-      const wrapperStyle = {
-        position: "fixed",
-        top: "1rem",
-        right: "0",
-        bottom: "0",
-        left: "0",
-        display: "none",
-        background: "black",
-        zIndex: 10000,
-        boxSizing: "border-box",
-        paddingTop: "1rem"
-      };
       const iframeStyle = {
         width: "100%",
         height: "100%"
       };
 
       applyStyle(ytApp.iframe, iframeStyle);
-      applyStyle(ytApp.wrapper, wrapperStyle);
       ytApp.wrapper.appendChild(ytApp.iframe);
+      ytApp.wrapper.classList("jk-yt-wrapper");
       document.body.appendChild(ytApp.wrapper);
 
-      const closeBtnStyle = {
-        position: "absolute",
-        right: 0,
-        top: 0,
-        border: 0,
-        margin: 0,
-        padding: "1rem",
-        lineHeight: 1,
-        borderRadius: "100%",
-        background: "rgba(255,255,255,0.3)"
-      };
       const closeBtn = document.createElement("button");
+      closeBtn.classList.add("jk-yt-close-btn");
       closeBtn.setAttribute("type", "button");
       closeBtn.addEventListener("click", () => ytApp.hide());
       closeBtn.innerHTML = "x";
