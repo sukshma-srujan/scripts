4625f38fa391969233d9dc5232efd57c7ebdffeb
Jitendra Kumar <jk.bhu85@gmail.com>
2023-12-14 15:39:36 +0530

    Update wikipedia-ui.js

--- wikipedia-ui.js
+++ wikipedia-ui.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wikipedia UI
 // @namespace    optimus29
-// @version      1.0
+// @version      1.1.0
 // @description  Change font, font size and appearance of top of page.
 // @author       Optimus Prime
 // @match        https://*.wikipedia.org/*
@@ -9,46 +9,52 @@
 // ==/UserScript==
 
 (function IIFE() {
-    'use strict';
-    
-    let sf = 'Fira Sans';
-    let sf2 = sf.split(' ').join('+');
-    
-    let mf = 'Fira Mono';
-    let mf2 = mf.split(' ').join('+');
-    
-    let css = 
+  'use strict';
+
+  const sf = 'Fira Sans';
+  const sf2 = sf.split(' ').join('+');
+
+  const mf = 'Fira Mono';
+  const mf2 = mf.split(' ').join('+');
+
+  const css =
 `
-@import url('https://fonts.googleapis.com/css?family=${sf2}:400,400i,600');
-@import url('https://fonts.googleapis.com/css?family=${mf2}:400,700');
-body{font-family: '${sf}', sans-serif;font-size: 24px;}
-pre, code, tt, kbd, samp, .mw-code{font-family:'${mf}', consolas, monospace;}
-code{font-weight: 400;}
-body table:first-of-type tr td:last-of-type{width: 100%;padding-left: 16px;}
+@import url('https://fonts.googleapis.com/css2?family=${mf2}&family=${sf2}:ital,wght@0,400;0,700;1,400&display=swap');
+:root {
+  --font-size-medium: 1.5rem;
+}
+body {
+  font-family: "${sf}", sans-serif;
+}
+pre,
+code,
+tt,
+kbd,
+samp,
+.mw-code {
+  font-family: "${mf}", consolas, monospace;
+}
+code {
+  font-weight: 400;
+}
+body table:first-of-type tr td:last-of-type {
+  width: 100%;
+  padding-left: 1rem;
+}
+.jk-infobox {
+  float: none;
+  width: 100%;
+  margin-left: 0;
+  margin-right: 0;
+  margin-bottom: 3rem;
+}
 `;
-    
-    
-    let style = document.createElement('style');
-    style.innerHTML = css;
-    document.head.appendChild(style);
-    
-    var tbls = document.querySelectorAll('body table');
-    var tbl, i;
-    
-    for (i = 0; tbls && i < tbls.length; i++) {
-        if ( tbls[i].classList.contains('infobox') ) {
-            tbl = tbls[i];
-            break;
-        }
-    }
-    
-    if ( tbl ) {
-        tbl.style.float = 'none';
-        tbl.style.width = '100%';
-        tbl.style.marginLeft = '0';
-        tbl.style.marginRight = '0';
-        tbl.style.marginBottom = '3em';
-    } else {
-        console.error("Table not found.");
-    }
+
+
+  const style = document.createElement('style');
+  style.innerHTML = css;
+  document.head.appendChild(style);
+
+
+  Array.from(document.querySelectorAll('body table.infobox')).forEach(e => e.classList.add("jk-infobox"));
 })();
