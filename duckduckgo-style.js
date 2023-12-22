cf20187bba381721d5057cfdb32596b36c95bd90
Jitendra Kumar <jk.bhu85@gmail.com>
2023-12-22 21:20:39 +0530

    Update duckduckgo-style.js

--- duckduckgo-style.js
+++ duckduckgo-style.js
@@ -2,7 +2,7 @@
 // @name         DuckDuckGo Style
 // @namespace    https://github.com/jkbhu85
 // @homepage     https://github.com/jkbhu85/scripts/blob/main/duckduckgo-style.js
-// @version      0.0.1
+// @version      0.0.2
 // @description  Change appearance of DuckDuckGo website.
 // @author       Jitendra Kumar
 // @match        https://duckduckgo.com/*
@@ -15,9 +15,15 @@
 
   const css = `
 @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,400;6..12,600&display=swap');
+:root {
+  --jk-font-family-01: "Nunito Sans", sans-serif;
+}
 html,body {
-  font-family: "Nunito Sans";
+  font-family: var(--jk-font-family-01);
   font-size: 1rem;
+}
+.wZ4JdaHxSAhGy1HoNVja {
+  font-family: var(--jk-font-family-01);
 }
 select, option, input, button {
   font-family: inherit;
