4ebc43e5f716f191f57b0dd7e210bf92e1a5f3b2
Jitendra Kumar <jk.bhu85@gmail.com>
2024-01-02 20:09:30 +0530

    Update github-style.js

--- github-style.js
+++ github-style.js
@@ -1,42 +1,74 @@
 // ==UserScript==
 // @name         Github Style
-// @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @namespace    https://github.com/jkbhu85
+// @homepage     https://github.com/jkbhu85/scripts/blob/main/github-style.js
+// @version      1.2.0
 // @description  Github Style
-// @author       You
+// @author       Jitendra Kumar
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function iipe() {
+  'use strict';
 
-    const css =
-`
+  const osDetector = {
+    isWindows: function() {
+      return navigator.userAgent.indexOf("Windows") > -1;
+    },
+    isMac: function() {
+      return navigator.userAgent.indexOf("Mac") > -1;
+    },
+    isLinux: function() {
+      return navigator.userAgent.indexOf("Linux") > -1;
+    }
+  };
+
+  const cssFontStyleWindows = `
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,200;0,400;0,600;1,400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
+:root {
+  --jk-ff: "Roboto", sans-serif;
+  --jk-mff: "Jetbrains Mono", ui-monospace,SFMono-Regular,SF Mono,Menlo,Consolas,Liberation Mono,monospace;
+}
+`.trim();
+
+  const cssFontStyleMac = `
 @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,200;0,400;0,600;1,400&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
 :root {
   --jk-ff: "IBM Plex Sans", sans-serif;
   --jk-mff: "Jetbrains Mono", ui-monospace,SFMono-Regular,SF Mono,Menlo,Consolas,Liberation Mono,monospace;
 }
+`.trim();
+
+  const cssFontImport = (function fontStyleSelector() {
+    if (osDetector.isWindows()) {
+      return cssFontStyleWindows;
+    }
+    return cssFontStyleMac;
+  })();
+
+  const css =
+`
+${cssFontImport}
 body {
   font-family: var(--jk-ff);
 }
 .markdown-body {
   font-family: var(--jk-ff);
 }
-pre {
-  font-family: var(--jk-mff);
+pre,
+.commit-ref,
+.blob-code-inner,
+.text-mono,
+.react-code-text {
+  font-family: var(--jk-mff) !important;
 }
-.commit-ref {
-  font-family: var(--jk-mff);
-}
-.blob-code-inner {
-  font-family: var(--jk-mff);
-}`;
+`.trim();
     const style = document.createElement('style');
     style.innerHTML = css;
     style.setAttribute("title", "jk_github_style");
-    document.body.appendChild(style);
+    document.head.appendChild(style);
 })();
