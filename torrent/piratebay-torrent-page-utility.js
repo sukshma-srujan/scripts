3707d61b17039394aeefdffdb89e0226668d9725
Jitendra Kumar <jk.bhu85@gmail.com>
2026-05-27 20:31:19 +0530

    Update piratebay-torrent-page-utility.js

--- torrent/piratebay-torrent-page-utility.js
+++ torrent/piratebay-torrent-page-utility.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Piratebay Torrent Page Utilities
 // @namespace    http://tampermonkey.net/optimus-prime
-// @version      1.2.0
+// @version      1.3.0
 // @description  Various utilities for torrent description page on thepiratebay and its proxies
 // @author       Optimus Prime
 // @match        *://thepiratebay.org/description.php?id=*
@@ -10,7 +10,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function ptpu() {
   'use strict';
   const _g = {
     urlPattern: /(?<domain>https?:\/\/[a-zA-Z0-9\-\.\_]+\.[a-zA-Z]+)(\/[a-zA-Z0-9\:\-\%\$\#\.]+)*/gm
@@ -52,9 +52,10 @@
     }
     descElem.appendChild(linkWrapper);
   }
+
   function addStyle() {
-     const css =
-           `.jk-section {
+    const css =
+          `.jk-section {
   margin: 1rem 0;
   padding: 1rem;
   box-shadow: 0 0 2px rgba(0,0,0,.2);
@@ -72,6 +73,63 @@
     style.innerHTML = css;
     document.body.appendChild(style);
   }
+
+  function createTorrentDownloadLinks() {
+    // check if hash value is present
+    const hashElem = document.querySelector(`#metadata label#ih`);
+    if (!hashElem) {
+      console.log("hash element not found");
+      return;
+    }
+    const hash = hashElem.innerText;
+    const hashPat = /[a-zA-Z0-9]+/
+    if (!hashPat.test(hash)) {
+      console.log("hash is not valid");
+      return;
+    }
+
+    // check if magnet link found
+    const magElem = document.querySelector(`.links #d a[href^="magnet"]`);
+    if (!magElem) {
+      console.log("magnet element not found");
+      return;
+    }
+    const links = magElem.parentNode;
+
+    // genereate download links
+    const torLinkData = [
+      {title:"iTorrent", url:`http://itorrents.org/torrent/${hash}.torrent`},
+      {title:"Torrage", url:`http://torrage.info/torrent.php?h=${hash}`},
+      {title:"BtCache", url:`http://btcache.me/torrent/${hash}`}
+    ];
+
+    // create anchors for download links
+    const anchorWrapper = document.createElement("div");
+    anchorWrapper.classList.add('jk-tor-anchor-wrapper');
+    const aStyle = document.createElement("style");
+    aStyle.innerHTML = `
+.jk-tor-anchor-wrapper {
+  margin-top: 1rem;
+}
+.jk-tor-anchor::before {
+  content: "\\21E9";
+  padding-right: 5px;
+}
+	`;
+    anchorWrapper.appendChild(aStyle);
+    links.appendChild(anchorWrapper);
+
+    for (let tLinkData of torLinkData) {
+      const a = document.createElement('a');
+      a.href = tLinkData.url;
+      a.innerHTML = tLinkData.title;
+      a.setAttribute("target", "_blank");
+      a.classList.add("jk-tor-anchor");
+      anchorWrapper.appendChild(a);
+    }
+  }
+
+  createTorrentDownloadLinks();
   makeLinks();
   addStyle();
 })();
